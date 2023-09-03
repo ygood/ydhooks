8f8a8b254109230585eccc4391e031867ed74561
yanlei <1239722456@qq.com>
2023-09-04 00:13:04 +0800

    feat: 项目初始化

---
+++ .dumirc.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  themeConfig: {
+    name: 'yd-hooks',
+    footer: 'Copyright © 2023 | Powered by yanlei',
+    socialLinks: {
+      github: 'https://github.com/ygood/ydhooks',
+    },
+  },
+  publicPath: '/ydhooks/',
+  base: '/ydhooks/',
+});
